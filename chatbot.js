e36f5b3bb07756f6d96dec354eb931f2e98c5707
Devin Gaughan <tigrandrago@gmail.com>
2016-09-28 08:16:28 -0700

    using arrays for multiple answers

--- chatbot.js
+++ chatbot.js
@@ -8,29 +8,51 @@ var responseArray = [ "Hello how are you", "Hi", "What brings you to wonderland?
 
 
   var talking = {
-  "Hey": responseArray[Math.floor(Math.random() * responseArray.length)],
-  "Pretty good": "Im glad to hear" ,
-  "C": "C is for the Chesire cat",
-  "D": "D is for the dark",
-  "E": 4,
-  "How old are you": "I am ....oh I can't help you sorry",
-  "Alice": "http://orig05.deviantart.net/c731/f/2015/074/0/5/alice_in_wonderland_chibi_by_starmasayume-d8hgr37.jpg",
-  "What will you do in life": "Probably be left in this computer till they wipe me out.",
-  "When will the world end": "that cannot be answered right now, maybe when I am an A.I.",
-  "J": "J is for Jelly",
-  "what time is it" : "The time is " + returnTime() +  "."
+  //"Hey":responseArray[response],
+  "hello": [ "Hello how are you", "Hi", "What brings you to wonderland?", "Fine", "AAAh! you scared me! I was sleeping",
+  "Hi! would you like some tea?"],
+  "Pretty good": ["Im glad to hear"],
+  "C": ["C is for the Chesire cat"],
+  "D": ["D is for the dark"],
+  "Whats very dear to your creator?": ["Well The anagram for her is Coe Nel, but sorry Im not aloud to tell!"],
+  "How old are you": ["I am ....oh I can't help you sorry"],
+  "Alice": ["<img src='alice.jpg' alt='alice'>"],
+  "What will you do in life": ["Probably be left in this computer till they wipe me out."],
+  "When will the world end": ["that cannot be answered right now, maybe when I am an A.I."],
+  "J": ["J is for Jelly"],
+  "what time is it" : ["The time is " + returnTime() +  "."]
   }
   var response;
   var greet;
+
+var fallback = ["Was that said right?", "I do not understand?", "Aah! what?!", "You sound like the hatter", ];
+
 
 
 
 function conversation() {
   greet =  document.getElementById('input').value; //stores the content in the chat-area
   response =  talking[greet]; // takes response and checks the object variable talking for a answer.
-  $("#chat-area").prepend("<div style='color: purple'>" + response + "</div></br>"); // places the response in
-$("#chat-area").prepend  ( greet + "</br>" ); // copies the process but with the
-document.getElementById('input').value = "";
+
+  if(response == undefined) {
+
+    fallbackresponse = fallback[Math.floor((Math.random() * fallback.length))];
+    $("#chat-area").prepend("<div style='color: purple'>" + fallbackresponse + "</div></br>")
+      $("#chat-area").prepend  ( greet + "</br>" );
+  }
+  else {
+    greetAnswer = response[Math.floor((Math.random() * response.length))];
+    $("#chat-area").prepend("<div style='color: purple'>" + greetAnswer + "</div></br>"); // places the response in
+  $("#chat-area").prepend  ( greet + "</br>" ); // copies the process but with the
+
+  }
+    document.getElementById('input').value = "";
+
+
+
+
+
+
 }
 
 function returnTime() {
